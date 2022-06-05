84fe928d33d61d1521cdbcc8616e6fe440e48404
hvlck <just.ethan@protonmail.com>
2022-06-05 18:24:59 -0400

    chore: update benchmark

--- benchmarks/simple/src/index.tsx
+++ benchmarks/simple/src/index.tsx
@@ -1,6 +1,4 @@
-import { build, append } from "../../../src";
-
-const time = new Date();
+import { build, append, mutate, mount, register, render } from "../../../src";
 
 const El = (i: number) => {
     const $el = build("div", String(i));
@@ -16,18 +14,38 @@ for (let i = 0; i < iterations; i++) {
 
 window.addEventListener("load", () => {
     const distance = iterations / 10;
+
+    const time = new Date();
+
     let i = 0;
     while (i < iterations) {
         let r = i;
-        setTimeout(() => {
-            append(document.body, ...items.slice(r, r + distance));
-        }, 0);
-
+        setTimeout(() => append(document.body, ...items.slice(r, r + distance)), 0);
         i += distance;
     }
 
     console.table({
         iterations,
-        time: new Date().getTime() - time.getTime(),
+        time_ms: new Date().getTime() - time.getTime(),
     });
+
+    setTimeout(() => {
+        const mutationTime = new Date();
+        let t = 0;
+        while (t < iterations) {
+            let r = t;
+            setTimeout(() =>
+                items
+                    .slice(r, r + distance)
+                    .forEach(i => mutate(i, { innerText: Number(i.innerText) + 1 }))
+            );
+
+            t += distance;
+        }
+
+        console.table({
+            iterations,
+            mutation_time_ms: new Date().getTime() - mutationTime.getTime(),
+        });
+    }, 1000);
 });
